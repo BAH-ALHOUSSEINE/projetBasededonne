57056fd7d4fb180683b9251d969827762a73396f
melvinbertrand <113372960+melvinbertrand@users.noreply.github.com>
2023-04-25 16:11:42 +0200

    Add files via upload

--- bdd/queries.sql
+++ bdd/queries.sql
@@ -1,4 +1,22 @@
 create view population_année_departement as
-Select d.code, SUM(stat.valeur) from StatCommuneAnnee stat, Commune c, Departement d
-where annee = @date_annee and id = "population" and c.departement = @codeDep
-group by Departement.code;
+Select SUM(stat.valeur) as pop, stat.annee, d.codeDep
+from StatCommuneAnnee stat, Departement d, Commune c
+where stat.id = 'population' and c.codeCom = stat.codeCom and c.codeDep = d.codeDep
+GROUP BY (d.codeDep, stat.annee);
+
+create view population_année_region as
+Select SUM(popDep.pop), d.codeReg, popDep.annee
+from population_année_departement as popDep, Departement d
+where popDep.codeDep = d.codeDep
+GROUP BY (d.codeReg, popDep.annee);
+
+
+drop view population_année_departement;
+drop view population_année_region;
+
+
+select * from population_année_departement vue
+where vue.annee = '2019' and vue.codeDep = '12';
+
+select * from population_année_region vue
+where vue.annee = '2019' and vue.codeReg = '76';
