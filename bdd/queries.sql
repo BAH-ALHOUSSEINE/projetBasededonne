e437a011430703e1cde1cd9489828793bc3b2142
melvinbertrand <113372960+melvinbertrand@users.noreply.github.com>
2023-04-26 15:19:58 +0200

    Add files via upload

--- bdd/queries.sql
+++ bdd/queries.sql
@@ -1,8 +1,18 @@
+--Vue population des départements
+
 create view population_année_departement as
 Select SUM(stat.valeur) as pop, stat.annee, d.codeDep
 from StatCommuneAnnee stat, Departement d, Commune c
 where stat.id = 'population' and c.codeCom = stat.codeCom and c.codeDep = d.codeDep
 GROUP BY (d.codeDep, stat.annee);
+
+drop view population_année_departement;
+
+select * from population_année_departement vue
+where vue.annee = '2019' and vue.codeDep = '12';
+
+
+--Vue population des régions
 
 create view population_année_region as
 Select SUM(popDep.pop), d.codeReg, popDep.annee
@@ -10,13 +20,16 @@ from population_année_departement as popDep, Departement d
 where popDep.codeDep = d.codeDep
 GROUP BY (d.codeReg, popDep.annee);
 
-
-drop view population_année_departement;
 drop view population_année_region;
-
-
-select * from population_année_departement vue
-where vue.annee = '2019' and vue.codeDep = '12';
 
 select * from population_année_region vue
 where vue.annee = '2019' and vue.codeReg = '76';
+
+
+--Ajout de population dans les tables régions et départements
+
+ALTER TABLE Departement
+ADD population INT;
+
+ALTER TABLE Region
+ADD population INT;
