5c85ce529d91485dbcf54675fd07acc704b47ad4
melvinbertrand <113372960+melvinbertrand@users.noreply.github.com>
2023-04-26 16:34:50 +0200

    Add files via upload

--- bdd/queries.sql
+++ bdd/queries.sql
@@ -15,14 +15,14 @@ where vue.annee = '2019' and vue.codeDep = '12';
 --Vue population des régions
 
 create view population_année_region as
-Select SUM(popDep.pop), d.codeReg, popDep.annee
+Select SUM(popDep.pop) as pop, d.codeReg, popDep.annee
 from population_année_departement as popDep, Departement d
 where popDep.codeDep = d.codeDep
 GROUP BY (d.codeReg, popDep.annee);
 
 drop view population_année_region;
 
-select * from population_année_region vue
+select vue.pop from population_année_region vue
 where vue.annee = '2019' and vue.codeReg = '76';
 
 
@@ -36,7 +36,24 @@ ADD population INT;
 
 --Procedure stocké
 
-CREATE PROCEDURE calculerSalaireMoyen(IN dep VARCHAR(3), OUT salaireMoyen DECIMAL(10,2))
-BEGIN
-    SELECT AVG(salaire_annuel) INTO salaireMoyen FROM employes WHERE departement = dep;
-END;
+--Departement
+CREATE PROCEDURE calculerPopulationDepartement
+()
+LANGUAGE SQL
+AS $$
+update Departement set population = (SELECT pop FROM population_année_departement vue WHERE vue.codeDep = Departement.codeDep and vue.annee = '2019');
+$$;
+
+call calculerPopulationDepartement();
+select population from Departement;
+
+--Region
+create Procedure calculerPopulationRegion
+()
+LANGUAGE SQL
+AS $$
+update Region set population = (SELECT pop FROM population_année_region vue WHERE vue.codeReg = Region.codeReg and vue.annee = '2019');
+$$;
+
+call calculerPopulationRegion();
+select population from Region;
